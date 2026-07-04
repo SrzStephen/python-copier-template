f9903bf1be014984d8ead2d6788bce996672a499
Stephen Mott <stephen@mott.email>
2026-07-04 12:31:28 +0000

    fix: always init git

--- template/.devcontainer/post-create.sh
+++ template/.devcontainer/post-create.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -euo pipefail
-if git rev-parse --git-dir &>/dev/null; then
-  git config pull.rebase true
-  git config rebase.autoStash true
-  git config init.defaultBranch main
-  git config alias.lg "log --oneline --graph --decorate --all"
-  git config alias.change "git log --format=format: --name-only --since='1 year ago' | sort | uniq -c | sort -nr | head -20"
-  git config color.ui auto
+if ! git rev-parse --git-dir &>/dev/null; then
+  git init
 fi
+git config pull.rebase true
+git config rebase.autoStash true
+git config init.defaultBranch main
+git config alias.lg "log --oneline --graph --decorate --all"
+git config alias.change "git log --format=format: --name-only --since='1 year ago' | sort | uniq -c | sort -nr | head -20"
+git config color.ui auto
 
 
 # The .cache/uv bind mount causes Docker to create /home/vscode/.cache as root-owned.
